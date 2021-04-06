ed74b55d1c273eb908c59dd6cb088686a5240620
HarshaMindfire17 <harsham@mindfiresolutions.com>
2021-04-06 12:58:46 +0530

    Final Assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { DisplayComponent } from './display/display.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RatingComponent } from './rating/rating.component';
+import { HttpClientModule } from '@angular/common/http'
+import { DataService } from './data.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DisplayComponent,
+    ProfileComponent,
+    RatingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
